4686008728c7df6d5a0db1a4bed00358ed2099d5
natbldavid <natbldavid@gmailcom>
2025-06-29 16:49:55 +0100

    Describe what changed

--- index.cjs
+++ index.cjs
@@ -53,14 +53,14 @@ app.post('/contact', async (req, res) => {
 
   try {
     await resend.emails.send({
-      from: '<Emelie.Hallett@yahoo.com>',
+      from: 'Emelie Hallett Music <noreply@emeliehallettmusic.co.uk>',
       to: process.env.RECEIVE_EMAIL,
       subject: 'New Contact Form Submission',
       html: `<h3>From: ${Name}</h3><p>Email: ${email}</p><p>Phone: ${phone}</p><p>${message}</p>`
     });
 
     await resend.emails.send({
-      from: '<Emelie.Hallett@yahoo.com>',
+      from: 'Emelie Hallett Music <noreply@emeliehallettmusic.co.uk>',
       to: email,
       subject: 'Thanks for contacting us',
       html: `<p>Hi ${Name}, thank you for your enquiry. We will respond within 30 working days.</p>`
@@ -84,7 +84,7 @@ app.post('/testimonials', async (req, res) => {
 
   try {
     await resend.emails.send({
-      from: '<Emelie.Hallett@yahoo.com>',
+      from: 'Emelie Hallett Music <noreply@emeliehallettmusic.co.uk>',
       to: process.env.RECEIVE_EMAIL,
       subject: 'New Testimonial Received',
       html: `${reviewHtml}`
